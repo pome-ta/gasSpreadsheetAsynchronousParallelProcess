03114a5df94260b5fdf8de83cdb717870cbe5172
pome-ta <pometarou93@gmail.com>
2022-07-02 23:05:49 +0900

    format

--- asynchronousParallelProcess/code.js
+++ asynchronousParallelProcess/code.js
@@ -5,17 +5,16 @@ function main() {
   console.log(spreadsheet.getName());
 }
 
-
-
-
 /* Spreadsheet 側のUI関係 */
 function onOpen() {
-  const entries = [{
-    name: '実行',
-    functionName: 'run'
-  }];
+  const entries = [
+    {
+      name: '実行',
+      functionName: 'run',
+    },
+  ];
   ss.addMenu('非同期並列処理', entries);
-};
+}
 
 function run() {
   const html = HtmlService.createTemplateFromFile('index').evaluate();
